dbd754807919a5c6733b5c153bfb850732e5dfee
Prosper Ikechukwu <mrhendrix62@gmail.com>
2021-10-10 19:58:53 +0100

    Added Projects section

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 const mainMenu = document.querySelector('.mainMenu');
 const closeMenu = document.querySelector('.closeMenu');
 const openMenu = document.querySelector('.openMenu');
-// const navLinks = document.querySelector('.links');
+const navLinks = document.querySelectorAll('.links');
 
 openMenu.addEventListener('click', show);
 closeMenu.addEventListener('click', close);
-// navLinks.addEventListener('click', close)
+
+mainMenu.addEventListener('click', function (e) {
+  if (e.target.className == 'links') {
+    mainMenu.classList.add('hide');
+    console.log(mainMenu.classList);
+  }
+})
 
 function show() {
   mainMenu.style.display = 'flex';
